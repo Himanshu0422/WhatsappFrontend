957d05cd151a31d5d8c5a7a8bd6bd19dc2ad78b5
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-13 10:46:52 +0530

    login done

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -26,6 +26,18 @@ export const registerUser = createAsyncThunk(
     }
 )
 
+export const loginUser = createAsyncThunk(
+    "auth/login",
+    async (values, { rejectWithValue }) => {
+        try {
+            const { data }=await axios.post('/auth/login', { ...values, });
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.response.data.error.message);
+        }
+    }
+)
+
 export const userSlice = createSlice({
     name: 'user',
     initialState,
@@ -42,6 +54,9 @@ export const userSlice = createSlice({
                 token: "",
             }
         },
+        changeStatus: (state, action) => {
+            state.status = action.payload
+        }
     },
     extraReducers (builder) {
         builder
@@ -56,9 +71,21 @@ export const userSlice = createSlice({
         .addCase (registerUser.rejected, (state, action) => {
             state.status = "failed";
             state.error = action.payload;
+        })
+        .addCase (loginUser.pending,(state, action) => {
+            state.status = "loading";
+        })
+        .addCase (loginUser.fulfilled, (state, action) => {
+            state.status = "succeeded";
+            state.error = "";
+            state.user = action.payload.user;
+        })
+        .addCase (loginUser.rejected, (state, action) => {
+            state.status = "failed";
+            state.error = action.payload;
         });
     }
 });
 
-export const { logout } = userSlice.actions;
+export const { logout, changeStatus } = userSlice.actions;
 export default userSlice.reducer;
