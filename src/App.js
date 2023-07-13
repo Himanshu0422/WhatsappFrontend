b11dedcad21caf30a7d107dc695e618e12371630
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-13 10:46:29 +0530

    picture component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function App() {
-	// const { user } = useSelector((state) => state.user); 
-	// console.log(user);
+	const { user } = useSelector((state) => state.user);
+	const { token } = user;
 	return (
 		<div className='dark'>
 			<Router>
 				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/register' element={<Register />} />
+					<Route path='/' element={token ? <Home /> : <Navigate to="/login" />} />
+					<Route path='/login' element={!token ? <Login /> : <Navigate to="/" />} />
+					<Route path='/register' element={!token ? <Register /> : <Navigate to="/" />} />
 				</Routes>
 			</Router>
 		</div>
