3ab8ea83ea87cbd3e6de54171c499c95481ab820
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-15 19:27:38 +0530

    get conversations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Sidebar } from '../components/sidebar'
+import { useDispatch, useSelector } from 'react-redux'
+import { getConversations } from '../features/chatSlice';
 
 export default function Home() {
-  return (
-    <div>Home Page</div>
-  )
+
+	const dispatch = useDispatch();
+	const { user } = useSelector((state) => state.user);
+	useEffect(() => {
+		if(user){
+			dispatch(getConversations(user.token));
+		}
+	}, [user])
+	
+
+	return (
+		<div className='min-h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+			<div className='container min-h-screen flex'>
+				<Sidebar />
+			</div>
+		</div>
+	)
 }
