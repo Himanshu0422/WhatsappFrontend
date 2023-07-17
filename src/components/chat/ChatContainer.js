2e15ddceaaaa9cac159b03becb4a039044eda1e4
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-17 10:57:20 +0530

    chat container

---
+++ src/components/chat/ChatContainer.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import ChatHeader from "./header/ChatHeader";
+import ChatMessage from './messages/ChatMessage';
+import { useDispatch, useSelector } from 'react-redux';
+import { getConversationMessages } from '../../features/chatSlice';
+
+export default function ChatContainer() {
+
+    const dispatch = useDispatch();
+    const { activeConversation, messages } = useSelector((state) => state.chat);
+    const { user } = useSelector((state) => state.user);
+    const { token } = user;
+    const values = {
+        token,
+        convo_id: activeConversation?._id
+    }
+    useEffect(() => {
+        if (activeConversation?._id) {
+            dispatch(getConversationMessages(values))
+        }
+    }, [activeConversation]);
+    console.log("messages", messages);
+
+    return (
+        <div className="relative w-full h-full border-1 dark:border-1-dark 2 select-none overflow-hidden">
+            <div>
+                <ChatHeader />
+                <ChatMessage />
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,29 @@ import React, { useEffect } from 'react'
 import { Sidebar } from '../components/sidebar'
 import { useDispatch, useSelector } from 'react-redux'
 import { getConversations } from '../features/chatSlice';
+import { ChatContainer, WhatsappHome } from '../components/chat' ;
 
 export default function Home() {
 
 	const dispatch = useDispatch();
 	const { user } = useSelector((state) => state.user);
+	const { activeConversation } = useSelector((state) => state.chat);
+	console.log("activeconversation",activeConversation);
 	useEffect(() => {
-		if(user){
+		if(user?.token){
 			dispatch(getConversations(user.token));
 		}
-	}, [user])
+	}, [user]);
 	
 
 	return (
 		<div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
 			<div className='container h-screen flex'>
 				<Sidebar />
+				{activeConversation?._id ? 
+					<ChatContainer /> :
+					<WhatsappHome />
+				}
 			</div>
 		</div>
 	)
