aca47ad6bd6b78e42f1bb70fad24f36f3d98587d
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-17 10:57:53 +0530

    message

---
+++ src/components/chat/messages/ChatMessage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import Message from './Message';
+
+export default function ChatMessage() {
+
+    const { messages } = useSelector((state) => state.chat);
+    const { user } = useSelector((state)=> state.user);
+
+    return (
+        <div className="mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-cover bg-no-repeat">
+            <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
+                {
+                    messages && messages.map((message) => 
+                        <Message message={message} key={message._id} me={user._id} />
+                    )
+                }
+            </div>
+        </div>
+    )
+}
