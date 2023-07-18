fb7ff47b2e86a7f972a1d13ec5205be6654eea64
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-18 11:17:26 +0530

    emijipicker and attachments component

---
+++ src/components/chat/actions/attachments/Attachments.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import { AttachmentIcon } from '../../../../svg'
+import Menu from './Menu'
+
+export default function Attachments({showAttachments, setShowAttachments, setShowPicker}) {
+    return (
+        <li className="relative">
+            <button type='button' className="btn" onClick={()=>{
+                setShowPicker(false)
+                setShowAttachments((prev) => !prev)
+            }}>
+                <AttachmentIcon className="dark:fill-dark_svg_1" />
+            </button>
+            {showAttachments ? <Menu /> : null}
+        </li>
+    )
+}
