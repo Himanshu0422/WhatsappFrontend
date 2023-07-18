fe33fc8ea107afc6b768f1c0cf74f204786c8859
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-18 11:18:24 +0530

    end scroll feature

--- src/components/chat/messages/ChatMessage.js
+++ src/components/chat/messages/ChatMessage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux';
 import Message from './Message';
 
@@ -6,15 +6,23 @@ export default function ChatMessage() {
 
     const { messages } = useSelector((state) => state.chat);
     const { user } = useSelector((state)=> state.user);
+    const endRef = useRef();
+    useEffect( ( ) => { 
+        scrollToBottom ();
+    }, [messages ]);
+    const scrollToBottom = () => {
+        endRef.current.scrollIntoView({behavior: "smooth" }) ;
+    };
 
     return (
-        <div className="mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-cover bg-no-repeat">
+        <div className="back mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')] bg-cover bg-no-repeat">
             <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
                 {
                     messages && messages.map((message) => 
-                        <Message message={message} key={message._id} me={user._id} />
+                        <Message message={message} key={message._id} me={user._id === message.sender._id} />
                     )
                 }
+                <div className='mt-2' ref={endRef}></div>
             </div>
         </div>
     )
