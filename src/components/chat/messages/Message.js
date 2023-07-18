106ea0d99229897899a40717ecae02053b7ae2dd
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-18 11:18:37 +0530

    message and input

--- src/components/chat/messages/Message.js
+++ src/components/chat/messages/Message.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import TraingleIcon from '../../../svg/triangle';
 
-export default function Message() {
-  return (
-    <div>Message</div>
-  )
+export default function Message({me, message}) {
+	return (
+		<div className={`w-full flex mt-2 space-x-3 max-w-xs ${me ? "ml-auto justify-end" : ""}`}>
+			<div>
+				<div className={`relative h-full dark:text-dark_text_1 p-2 rounded-lg ${me ? "bg-green_3" : "dark:bg-dark_bg_2"}`}>
+					<p className='float-left h-full text-sm pb-4 pr-8'>{message.message}</p>
+					<span className='absolute right-1.5 bottom-1.5 text-xs text-dark_text_5 leading-none'>
+						{moment(message.createdAt).format('HH:mm')}
+					</span>
+					{!me ? (
+						<span>
+							<TraingleIcon className='dark:fill-dark_bg_2 rotate-[60deg] absolute top-[-5px] -left-1.5' />
+						</span>) : 
+						null
+					}
+				</div>
+			</div>
+		</div>
+	)
 }
