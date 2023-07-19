87e66f207455070a2c419e2a31d717c168e9f345
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-19 07:10:14 +0530

    socket imported

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
+import SocketContext from './context/SocketContext';
+
+const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
 
 function App() {
 	const { user } = useSelector((state) => state.user);
 	const { token } = user;
 	return (
 		<div className='dark'>
-			<Router>
-				<Routes>
-					<Route path='/' element={token ? <Home /> : <Navigate to="/login" />} />
-					<Route path='/login' element={!token ? <Login /> : <Navigate to="/" />} />
-					<Route path='/register' element={!token ? <Register /> : <Navigate to="/" />} />
-				</Routes>
-			</Router>
+			<SocketContext.Provider value={socket}>
+				<Router>
+					<Routes>
+						<Route path='/' element={token ? <Home socket={socket} /> : <Navigate to="/login" />} />
+						<Route path='/login' element={!token ? <Login /> : <Navigate to="/" />} />
+						<Route path='/register' element={!token ? <Register /> : <Navigate to="/" />} />
+					</Routes>
+				</Router>
+			</SocketContext.Provider>
 		</div>
 	);
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,55 @@
-import React, { useEffect } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
 import { Sidebar } from '../components/sidebar'
 import { useDispatch, useSelector } from 'react-redux'
-import { getConversations } from '../features/chatSlice';
+import { getConversations, updateMessages } from '../features/chatSlice';
 import { ChatContainer, WhatsappHome } from '../components/chat' ;
+import SocketContext from '../context/SocketContext';
 
-export default function Home() {
+function Home({ socket }) {
 
 	const dispatch = useDispatch();
 	const { user } = useSelector((state) => state.user);
 	const { activeConversation } = useSelector((state) => state.chat);
-	console.log("activeconversation",activeConversation);
+	const [onlineUsers, setOnlineUsers] = useState([]);
+	const [typing, setTyping] = useState(false);
 	useEffect(() => {
-		if(user?.token){
-			dispatch(getConversations(user.token));
-		}
+		socket.emit('join', user._id);
+		socket.on('get-online-users', (users)=>{
+			setOnlineUsers(users);
+		})
+	}, [user])
+	useEffect(() => {
+	if(user?.token){
+		dispatch(getConversations(user.token));
+	}
+	}, [user]);
+	useEffect(() => {
+		socket.on('receive message', (message) => {
+			dispatch(updateMessages(message));
+		})
+		socket.on("typing", (conversation) => setTyping(conversation));
+		socket.on("stop typing", () => setTyping(false));
 	}, [user]);
 	
 
 	return (
 		<div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center pt-[1px] overflow-hidden'>
 			<div className='container h-screen flex'>
-				<Sidebar />
+				<Sidebar onlineUsers={onlineUsers} typing={typing} />
 				{activeConversation?._id ? 
-					<ChatContainer /> :
+					<ChatContainer onlineUsers={onlineUsers} typing={typing} /> :
 					<WhatsappHome />
 				}
 			</div>
 		</div>
 	)
 }
+
+const HomeWithSocket=(props)=>(
+	<SocketContext.Consumer>
+		{(socket) => <Home {...props} socket={socket} />}
+	</SocketContext.Consumer>
+)
+
+export default HomeWithSocket;
