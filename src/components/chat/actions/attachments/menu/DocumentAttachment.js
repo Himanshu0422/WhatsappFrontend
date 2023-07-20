4045b0b587cdf4a5ba920e29ff7c61210357b37f
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-20 11:43:48 +0530

    document attachment

---
+++ src/components/chat/actions/attachments/menu/DocumentAttachment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocumentIcon } from '../../../../../svg'
+
+export default function DocumentAttachment() {
+    return (
+        <li>
+            <button type="button" className="bg-[#5F66CD] rounded-full">
+                <DocumentIcon />
+            </button>
+        </li>
+    )
+}
