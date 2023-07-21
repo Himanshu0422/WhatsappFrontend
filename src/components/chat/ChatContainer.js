9ce15ca0567989c73ec9298a345ac2219618d9ed
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-21 14:37:05 +0530

    file preview

--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -5,11 +5,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getConversationMessages } from '../../features/chatSlice';
 import ChatActions from './actions/ChatActions';
 import { checkOnlineStatus } from '../../utils/chat';
+import FilesPreview from './preview/files/FilesPreview';
 
 export default function ChatContainer({ onlineUsers, typing }) {
 
     const dispatch = useDispatch();
-    const { activeConversation } = useSelector((state) => state.chat);
+    const { activeConversation, files } = useSelector((state) => state.chat);
     const { user } = useSelector((state) => state.user);
     const { token } = user;
     const values = {
@@ -27,8 +28,13 @@ export default function ChatContainer({ onlineUsers, typing }) {
         <div className="relative w-full h-full border-1 dark:border-1-dark 2 select-none overflow-hidden">
             <div>
                 <ChatHeader online={checkOnlineStatus(onlineUsers,user, activeConversation.users)} />
-                <ChatMessage typing={typing} />
-                <ChatActions />
+                {files.length>0 ?
+                    <FilesPreview /> : 
+                    <>
+                        <ChatMessage typing={typing} />
+                        <ChatActions />
+                    </>
+                }
             </div>
         </div>
     )
